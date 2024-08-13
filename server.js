04569baf535ce3fe4efae6e72504bb6e50316730
Nguyễn Thái Minh <nguyenthaiminh0973200965@gmail.com>
2024-08-13 21:00:06 +0700

    Commit files

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
 const express = require('express');
+const fetch = require('node-fetch'); // Nếu bạn sử dụng Node.js 18+, fetch đã được tích hợp sẵn
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
-const rootDir = path.join(__dirname);
 
-app.use(express.static(rootDir));
-app.get('/file-tree', (req, res) => {
-    const fileTree = buildFileTree(rootDir);
-    res.json(fileTree);
-});
+const GITHUB_REPO = 'https://api.github.com/repos/thaiminhnguyen1999/HiTV/contents/';
+const GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
-app.get('/content.html', (req, res) => {
-    const filePath = path.join(rootDir, req.query.file);
-    if (fs.existsSync(filePath)) {
-        res.sendFile(path.join(rootDir, 'content.html'));
-    } else {
-        res.status(404).send('File not found');
+app.use(express.static(__dirname));
+
+app.get('/file-tree', async (req, res) => {
+    try {
+        const response = await fetch(GITHUB_REPO, {
+            headers: {
+                'Authorization': `token ${GITHUB_TOKEN}`
+            }
+        });
+        const data = await response.json();
+
+        const filteredData = data.filter(file => !['index.html', 'content.html', 'server.js', 'package.json', 'package-lock.json'].includes(file.name));
+
+        res.json(buildFileTree(filteredData));
+    } catch (error) {
+        console.error('Error fetching repository content:', error);
+        res.status(500).send('Error fetching repository content');
     }
 });
 
-function buildFileTree(dir, baseDir = '') {
+function buildFileTree(items) {
     const fileTree = {};
-    fs.readdirSync(dir).forEach(file => {
-        const filePath = path.join(dir, file);
-        const relativePath = path.join(baseDir, file);
-        if (fs.statSync(filePath).isDirectory()) {
-            fileTree[file] = buildFileTree(filePath, relativePath);
+    items.forEach(item => {
+        if (item.type === 'dir') {
+            fileTree[item.name] = {};
         } else {
-            fileTree[file] = relativePath;
+            fileTree[item.name] = item.path;
         }
     });
     return fileTree;
 }
+
+app.get('/content.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'content.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
