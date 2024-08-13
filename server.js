73e5583be07d38592437c1335f1a4477989a7567
Nguyễn Thái Minh <nguyenthaiminh0973200965@gmail.com>
2024-08-13 20:44:26 +0700

    Commit file

--- server.js
+++ server.js
@@ -7,27 +7,20 @@ const PORT = 3000;
 const rootDir = path.join(__dirname);
 
 app.use(express.static(rootDir));
-
-// Route để cung cấp cây thư mục dưới dạng JSON
 app.get('/file-tree', (req, res) => {
     const fileTree = buildFileTree(rootDir);
     res.json(fileTree);
 });
 
-// Route để phục vụ nội dung tệp
-app.get('/content', (req, res) => {
+app.get('/content.html', (req, res) => {
     const filePath = path.join(rootDir, req.query.file);
-
-    // Kiểm tra nếu file tồn tại
     if (fs.existsSync(filePath)) {
-        // Gửi lại nội dung của trang content.html để hiển thị file
         res.sendFile(path.join(rootDir, 'content.html'));
     } else {
         res.status(404).send('File not found');
     }
 });
 
-// Hàm xây dựng cây thư mục
 function buildFileTree(dir, baseDir = '') {
     const fileTree = {};
     fs.readdirSync(dir).forEach(file => {
